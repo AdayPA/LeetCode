c640b3d632bb5e445dcad6f9b8549251b7cc61e8
AdayPA <34550024+AdayPA@users.noreply.github.com>
2023-11-15 23:16:54 +0000

    Time: 27 ms (33.22%), Space: 71.7 MB (51.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,34 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-         ListNode *dummyHead = new ListNode(0); // Dummy node to simplify the code
-    ListNode *current = dummyHead;
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* dummyHead = new ListNode(0); // Dummy node to simplify the code
+    ListNode* current = dummyHead;
     int carry = 0;
 
-    while (l1 != nullptr || l2 != nullptr) {
-        // Get the values of the current nodes (if available)
-        int x = (l1 != nullptr) ? l1->val : 0;
-        int y = (l2 != nullptr) ? l2->val : 0;
+    while (l1 || l2 || carry) {
+        int x = l1 ? l1->val : 0;
+        int y = l2 ? l2->val : 0;
 
-        // Calculate the sum and carry
         int sum = x + y + carry;
         carry = sum / 10;
 
-        // Create a new node with the digit of the sum
         current->next = new ListNode(sum % 10);
         current = current->next;
 
-        // Move to the next nodes (if available)
-        if (l1 != nullptr) l1 = l1->next;
-        if (l2 != nullptr) l2 = l2->next;
-    }
-
-    // Handle any remaining carry
-    if (carry > 0) {
-        current->next = new ListNode(carry);
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
     }
 
     return dummyHead->next;
-        }
+}
+
 };
