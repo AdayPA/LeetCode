499ab38c1bd12c8e50b105eb19c66de4acdbd6d1
AdayPA <34550024+AdayPA@users.noreply.github.com>
2023-11-15 18:46:24 +0000

    Time: 11 ms (69.07%), Space: 11.5 MB (19.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if ( i == j) {
-                    j++;
-                } else {
-                    if ( (nums[i]+nums[j]) == target ) {
-                        result.push_back(i);
-                        result.push_back(j);
-                        return result;
-                    }
-                }
+    std::vector<int> twoSum(const std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> num_indices;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complemento = target - nums[i];
+            if (num_indices.find(complemento) != num_indices.end()) {
+                return {num_indices[complemento], i};
             }
-
+            num_indices[nums[i]] = i;
         }
-        return result;
+        return {};
     }
 };
