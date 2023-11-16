b7f688f2092c3f8b36dfbd83568cef8e69eb39a0
AdayPA <34550024+AdayPA@users.noreply.github.com>
2023-11-16 18:30:06 +0000

    Time: 55 ms (5.64%), Space: 90.7 MB (5.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,60 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       /////////////////////////////
-
-    int max_size = nums1.size() + nums2.size();
+    // reduced space
+    int totalSize = nums1.size() + nums2.size();
     int p1 = 0;
     int p2 = 0;
-    double temp = 0;
     vector<int> result;
 
-    while (p1 < nums1.size() && p2 < nums2.size())
-    {
-        if (nums1[p1] <= nums2[p2])
-        {
+    while (p1 < nums1.size() && p2 < nums2.size()) {
+        if (nums1[p1] <= nums2[p2]) {
             result.push_back(nums1[p1]);
             p1++;
-        }
-        else
-        {
+        } else {
             result.push_back(nums2[p2]);
             p2++;
         }
     }
 
-    // Añadir elementos restantes de nums1, si los hay
-    while (p1 < nums1.size())
-    {
+    while (p1 < nums1.size()) {
         result.push_back(nums1[p1]);
         p1++;
     }
 
-    // Añadir elementos restantes de nums2, si los hay
-    while (p2 < nums2.size())
-    {
+    while (p2 < nums2.size()) {
         result.push_back(nums2[p2]);
         p2++;
     }
 
-
-    if (result.size() % 2 == 0)
-    {
-
-        temp = static_cast<double>(result[(result.size() - 1) / 2] + result[result.size() / 2]) / 2;
-
-        
-        return temp;
+    // Imprimir el resultado
+    cout << "Merged Vector: ";
+    for (int num : result) {
+        cout << num << " ";
     }
-    else
-    {
-        temp = result[result.size() / 2];
-        
-        return temp;
+    cout << endl;
+
+    // Calcular y devolver la mediana
+    if (totalSize % 2 == 0) {
+        return (result[(totalSize - 1) / 2] + result[totalSize / 2]) / 2.0;
+    } else {
+        return result[totalSize / 2];
     }
-        
-        
-        
-        ///////////////////////////////////////
     }
 };
