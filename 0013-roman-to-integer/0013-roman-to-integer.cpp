6fddeb216bebd1c428dcda80019ae5ab378a6560
AdayPA <34550024+AdayPA@users.noreply.github.com>
2023-11-23 20:24:13 +0000

    Time: 11 ms (56.27%), Space: 8.6 MB (27.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-int romanToInt(string s)
-{
-    map<char, int> mymap = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+int romanToInt(const std::string& s) {
+    std::map<char, int> mymap = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
     int value = 0;
     bool getTuple = false;
-    for (size_t i = 0; i < s.length(); i+= (getTuple ? 2 : 1)){
-        getTuple = false;
-        auto inc_1 = mymap.find(s.at(i));
-        value += inc_1->second;
-        if ((i+1) < s.length()){
-            auto inc_2 = mymap.find(s.at(i+1));
-            if (inc_1->second < inc_2->second) {
-                value -= inc_1->second;
-                value += inc_2->second - inc_1->second;
+
+    const size_t length = s.length();
+    size_t i = 0;
+
+    while (i < length) {
+        const auto& inc_1 = mymap.at(s[i]);
+        value += inc_1;
+
+        if ((i + 1) < length) {
+            const auto& inc_2 = mymap.at(s[i + 1]);
+            if (inc_1 < inc_2) {
+                value -= inc_1;
+                value += inc_2 - inc_1;
                 getTuple = true;
-            } 
+            }
         }
+
+        i += (getTuple ? 2 : 1);
+        getTuple = false;
     }
+
     return value;
 }
 };
