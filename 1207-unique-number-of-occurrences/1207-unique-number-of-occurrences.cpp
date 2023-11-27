4629e56fbb54f6069a704a4f91eb0c1c37827232
AdayPA <34550024+AdayPA@users.noreply.github.com>
2023-11-27 21:00:22 +0000

    Time: 0 ms (100.00%), Space: 8.7 MB (21.13%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-bool uniqueOccurrences(vector<int> &arr)
+bool uniqueOccurrences(const std::vector<int> &arr)
 {
-    map<int, int> m;
-    std::map<int, int> aux_map;
-    for (int i = 0; i < arr.size(); i++)
+    std::unordered_map<int, int> frequencyMap;
+
+    for (int num : arr)
     {
-        m[arr[i]]++;
+        frequencyMap[num]++;
     }
-    for (const auto &pair : m)
-        ++aux_map[pair.second];
-    const int max_frequency = !aux_map.empty() ? aux_map.rbegin()->first : 0;
-    for (const auto &pair : m)
+
+    std::unordered_set<int> uniqueFrequencies;
+    for (const auto &pair : frequencyMap)
     {
-        std::cout << pair.first << ' ' << pair.second;
-        const int num_times = aux_map[pair.second];
-        if (num_times > 1)
+        if (!uniqueFrequencies.insert(pair.second).second)
+        {
+
             return false;
+        }
     }
+
     return true;
 }
 };
